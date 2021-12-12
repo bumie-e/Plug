7bf5eb7009372f9072df88b0b7b00310095fa27b
Amuke Sunday <sundayamuke7@gmail.com>
2021-12-12 17:18:55 +0100

    Added a Vendor model class

---
+++ lib/model/vendor.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Vendor{
+  final String id;
+  final String logoUrl;
+  final String businessName;
+  final String businessEmail;
+  final String whatsappNumber;
+  final String address;
+  final String openingHours;
+  final String closingHours;
+  final String desc;
+
+  Vendor({
+    required this.id,
+    required this.logoUrl,
+    required this.businessName,
+    required this.businessEmail,
+    required this.whatsappNumber,
+    required this.address,
+    required this.openingHours,
+    required this.closingHours,
+    required this.desc
+  });
+
+  factory Vendor.fromDocument(DocumentSnapshot doc) {
+    return Vendor(
+      id: doc.id,
+      logoUrl: doc['logoUrl'],
+      businessName: doc['businessName'],
+      businessEmail: doc['businessEmail'],
+      whatsappNumber: doc['whatsappNumber'],
+      address: doc['address'],
+      openingHours: doc['openingHours'],
+      closingHours: doc['closingHours'],
+      desc: doc['description']
+    );
+  }
+}
